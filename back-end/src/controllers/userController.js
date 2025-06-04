68702f44607993ec46139ff8672288f9c8244bb3
BuiAnhTai_21020981 <buianhtai4903@gmail.com>
2025-06-04 22:56:08 +0700

    login

--- back-end/src/controllers/userController.js
+++ back-end/src/controllers/userController.js
@@ -15,7 +15,17 @@ exports.register = async (req, res) => {
         })
     }
 }
-// exports.login = async (req, res) => {
-//     const ( email, password) = req.body;
-//     const result = await
-// }
+exports.login = async (req, res) => {
+    const { email, password } = req.body;
+    const result = await userService.login(email, password);
+    if (!result) {
+        return res.status(400).json({
+            result
+        })
+    }
+    else {
+        return res.status(200).json({
+            result
+        })
+    }
+}
--- back-end/src/services/userServices.js
+++ back-end/src/services/userServices.js
@@ -1,3 +1,4 @@
+const bcrypt = require('bcryptjs');
 const prisma = require('../../prisma/client');
 exports.register = async (email, password, full_name) => {
     const user = await prisma.users.findUnique({
@@ -11,13 +12,51 @@ exports.register = async (email, password, full_name) => {
         return null;
     }
 
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
     const result = await prisma.users.create({
         data: {
             email: email,
-            password: password,
+            password: hashedPassword,
             full_name: full_name
         }
     })
 
     return result;
+}
+
+exports.login = async (email, password) => {
+    const user = await prisma.users.findUnique({
+        where: {
+            email: email
+        }
+    })
+    if (!user) {
+        console.log('khong tim thay user');
+        return {
+            message: 'tai khoan khong ton tai',
+            data: null
+        };
+    }
+    else {
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            console.log('Dang nhap thanh cong');
+            return {
+                message: 'Dang nhap thanh cong',
+                data: {
+                    username: user.full_name,
+                    usermail: user.email
+                }
+            };
+        }
+        else {
+            console.log('Mat khau khong dung');
+            return {
+                message: 'mat khau khong dung',
+                data: null
+            };
+        }
+    }
 }
